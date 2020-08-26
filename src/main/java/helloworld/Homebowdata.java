4d98f41bed6c056b85ec6ca59dd1a7bca35c0a29
rtan37 <rtan37@gatech.edu>
2020-08-25 18:20:48 -0700

    modify table

--- src/main/java/helloworld/Homebowdata.java
+++ src/main/java/helloworld/Homebowdata.java
@@ -8,35 +8,83 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 public class Homebowdata {
 
     private String mlsId;
-    private String name;
     private String type;
-//    private Address address;
+    private String parking;
+    private String yearBuilt;
+    private double hoa;
+    private String heating;
+    private String cooling;
+    private double priceSqft;
 
-    //primary key is propertyId
     @DynamoDBHashKey(attributeName = "mlsId")
-    public String getMlsId() { return mlsId;}
-
-    @DynamoDBAttribute(attributeName = "name")
-    public String getName() { return name;}
+    public String getMlsId() {
+        return mlsId;
+    }
 
     @DynamoDBAttribute(attributeName = "type")
-    public String getType() { return type;}
+    public String getType() {
+        return type;
+    }
 
-//    @DynamoDBAttribute(attributeName = "address")
-//    public Address getAddress() {return address;}
+    @DynamoDBAttribute(attributeName = "parking")
+    public String getParking() {
+        return parking;
+    }
+
+    @DynamoDBAttribute(attributeName = "yearBuilt")
+    public String getYearBuilt() {
+        return yearBuilt;
+    }
+
+    @DynamoDBAttribute(attributeName = "hoa")
+    public double getHoa() {
+        return hoa;
+    }
+
+    @DynamoDBAttribute(attributeName = "heating")
+    public String getHeating() {
+        return heating;
+    }
+
+    @DynamoDBAttribute(attributeName = "cooling")
+    public String getCooling() {
+        return cooling;
+    }
+
+    @DynamoDBAttribute(attributeName = "priceSqft")
+    public double getPriceSqft() {
+        return priceSqft;
+    }
 
     public void setMlsId(String mlsId) {
         this.mlsId = mlsId;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setType(String type) { this.type = type; }
+
+    public void setParking(String parking) {
+        this.parking = parking;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setYearBuilt(String yearBuilt) {
+        this.yearBuilt = yearBuilt;
     }
-//    public void setAddress(Address address) {
-//        this.address = address;
-//    }
+
+    public void setHoa(double hoa) {
+        this.hoa = hoa;
+    }
+
+    public void setHeating(String heating) {
+        this.heating = heating;
+    }
+
+    public void setCooling(String cooling) {
+        this.cooling = cooling;
+    }
+
+    public void setPriceSqft(double priceSqft) {
+        this.priceSqft = priceSqft;
+    }
+
 }
+
